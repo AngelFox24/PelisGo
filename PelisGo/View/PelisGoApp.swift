4726913bca9186ea49b95462fb84400e117a5622
AngelFox24 <curilaurente@gmail.com>
2023-11-23 13:15:03 -0500

    Inicio del proyecto
    Se limpian las lineas generadas automáticamente.
    Se crean carpetas para organizar los archivos

--- PelisGo/View/PelisGoApp.swift
+++ PelisGo/View/PelisGoApp.swift
@@ -14,7 +14,6 @@ struct PelisGoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
